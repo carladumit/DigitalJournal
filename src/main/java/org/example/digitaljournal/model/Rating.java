144275db86c3ca867441bf333d4e536f03f49401
Carla <carladumit@gmail.com>
2026-07-20 16:37:32 +0200

    feat: added model classes

---
+++ src/main/java/org/example/digitaljournal/model/Rating.java
@@ -0,0 +1,15 @@
+package org.example.digitaljournal.model;
+
+public enum Rating {
+    SAD, BAD, NEUTRAL, GOOD, GREAT;
+
+    public String toEmoji() {
+        return switch (this) {
+            case SAD -> "😭";
+            case BAD -> "🙁";
+            case NEUTRAL -> "😐";
+            case GOOD -> "🙂";
+            case GREAT -> "😄";
+        };
+    }
+}
