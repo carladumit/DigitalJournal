3cf748231ba9af10049c3b5cd6340e7794033e16
Carla <carladumit@gmail.com>
2026-07-20 19:08:05 +0200

    feat: implement hash encryption

---
+++ src/main/java/org/example/digitaljournal/util/PasswordHash.java
@@ -0,0 +1,17 @@
+package org.example.digitaljournal.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHash {
+
+    public static String createHash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkHash(
+            String password,
+            String hash) {
+
+        return BCrypt.checkpw(password, hash);
+    }
+}
