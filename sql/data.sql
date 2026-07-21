fa5bd51449e5e2d19382bf12fc3d9e59417b4bd6
Carla <carladumit@gmail.com>
2026-07-21 16:49:44 +0200

    feat: create data for testing

---
+++ sql/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (username, password) VALUES
+('clever_fox', 'Fox#Pass92'),
+('brave_bear', 'Bear!Secure7'),
+('chilly_penguin', 'Penguin$44'),
+('silent_owl', 'NightOwl#18'),
+('happy_dolphin', 'Splash!2026'),
+('sleepy_sloth', 'SlowTree*55'),
+('mighty_lion', 'Roar#Power1'),
+('swift_falcon', 'FlyFast!88'),
+('calm_koala', 'Eucalyptus$3'),
+('curious_otter', 'River#Fun22');
