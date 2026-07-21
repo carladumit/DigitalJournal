9fd87374f70fd871d4289d3e834fe29eb4c0f345
Carla <carladumit@gmail.com>
2026-07-21 19:47:04 +0200

    feat: Main class

--- src/main/java/org/example/digitaljournal/Main.java
+++ src/main/java/org/example/digitaljournal/Main.java
@@ -1,4 +1,11 @@
 package org.example.digitaljournal;
 
+import org.example.digitaljournal.ui.ConsoleUI;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        ConsoleUI.start();
+    }
 }
