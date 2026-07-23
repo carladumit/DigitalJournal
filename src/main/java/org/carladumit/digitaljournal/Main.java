0a54c7104fa09aeeac936433e4ef4ee3eccd6fff
Carla <carladumit@gmail.com>
2026-07-23 19:27:57 +0200

    refactor: unused import

--- src/main/java/org/carladumit/digitaljournal/Main.java
+++ src/main/java/org/carladumit/digitaljournal/Main.java
@@ -2,10 +2,8 @@ package org.carladumit.digitaljournal;
 
 import org.carladumit.digitaljournal.ui.ConsoleUI;
 
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         ConsoleUI.start();
     }
 }
